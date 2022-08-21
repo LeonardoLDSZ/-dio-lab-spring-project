3dacd49734aaa286a9c31bf8aeaca7f98790acf7
LeonardoLDSZ <leonardodavidesouza@gmail.com>
2022-08-21 16:20:45 -0300

    first commit

---
+++ src/main/java/digitalinnovationone/diolabspringproject/DioLabSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package digitalinnovationone.diolabspringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioLabSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioLabSpringProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/digitalinnovationone/diolabspringproject/DioLabSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package digitalinnovationone.diolabspringproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioLabSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
